031db5609a4a0e665c8286577c043932aae822ea
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-02-04 15:03:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './style.css';
 import { AdvocateList, CourtList, ClientList } from './components/index';
+const YOUR_API_KEY = "keyeNXyxxuuYJY19w";
 export default function App() {
+  const movies = useState([])
+  useEffect(){
+    fetch(`https://api.airtable.com/v0/app5F4nbMoQ4mxv4N/Advocates?api_key=${YOUR_API_KEY}`)
+    .then((resp) => resp.json())
+    .then(data => {
+       this.setState({ movies: data.records });
+    }).catch(err => {
+      // Error :(
+    });
+  }
+  console.log(movies)
+
   return (
     <div>
       <h1>Hello world</h1>
